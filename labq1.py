8d6d13cee6dc553b5ddfd9faf28b00eec75a95f4
Kavitasonawne <166548548+Kavitasonawne@users.noreply.github.com>
2025-02-12 12:34:16 +0530

    Add files via upload

---
+++ labq1.py
@@ -0,0 +1,10 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1=3
+>>> num2=4
+>>> multiplication_result=num1*num2
+>>> sum_result=num1+num2
+>>> print(f"multiplication:{multiplication_result}")
+multiplication:12
+>>> print(f"sum:{sum_result}")
+sum:7
---
+++ labq2.py
@@ -0,0 +1,10 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>  num1=3
+...  
+SyntaxError: unexpected indent
+>>> num1=3
+>>> num2=4
+>>> largest = num1 if num1 > num2 else num2
+>>> print(f"The largest number is: {largest}")
+The largest number is: 4
